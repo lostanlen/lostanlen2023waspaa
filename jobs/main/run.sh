a66829cfc353f5670edf3d9f4a98edc436c5a22a
Vincent Lostanlen <vl1019@log-1.hpc.nyu.edu>
2023-04-26 06:12:14 -0400

    add sbatch files

--- jobs/main/run.sh
+++ jobs/main/run.sh
@@ -4,7 +4,7 @@
 # Job Batch: main
 # Params:
 # [
-#   ('domain', ['speech']),
+#   ('domain', ['music', 'speech', 'synth', 'urban']),
 #   ('arch', ['TDFilterbank', 'LEAF', 'MuReNN']),
 #   ('init_id', range(0, 5)),
 # ]
@@ -13,6 +13,21 @@
 
 
 
+sbatch "jobs/main/main,domain-music,arch-TDFilterbank,init_id-0.sbatch"
+sbatch "jobs/main/main,domain-music,arch-TDFilterbank,init_id-1.sbatch"
+sbatch "jobs/main/main,domain-music,arch-TDFilterbank,init_id-2.sbatch"
+sbatch "jobs/main/main,domain-music,arch-TDFilterbank,init_id-3.sbatch"
+sbatch "jobs/main/main,domain-music,arch-TDFilterbank,init_id-4.sbatch"
+sbatch "jobs/main/main,domain-music,arch-LEAF,init_id-0.sbatch"
+sbatch "jobs/main/main,domain-music,arch-LEAF,init_id-1.sbatch"
+sbatch "jobs/main/main,domain-music,arch-LEAF,init_id-2.sbatch"
+sbatch "jobs/main/main,domain-music,arch-LEAF,init_id-3.sbatch"
+sbatch "jobs/main/main,domain-music,arch-LEAF,init_id-4.sbatch"
+sbatch "jobs/main/main,domain-music,arch-MuReNN,init_id-0.sbatch"
+sbatch "jobs/main/main,domain-music,arch-MuReNN,init_id-1.sbatch"
+sbatch "jobs/main/main,domain-music,arch-MuReNN,init_id-2.sbatch"
+sbatch "jobs/main/main,domain-music,arch-MuReNN,init_id-3.sbatch"
+sbatch "jobs/main/main,domain-music,arch-MuReNN,init_id-4.sbatch"
 sbatch "jobs/main/main,domain-speech,arch-TDFilterbank,init_id-0.sbatch"
 sbatch "jobs/main/main,domain-speech,arch-TDFilterbank,init_id-1.sbatch"
 sbatch "jobs/main/main,domain-speech,arch-TDFilterbank,init_id-2.sbatch"
@@ -28,4 +43,34 @@ sbatch "jobs/main/main,domain-speech,arch-MuReNN,init_id-1.sbatch"
 sbatch "jobs/main/main,domain-speech,arch-MuReNN,init_id-2.sbatch"
 sbatch "jobs/main/main,domain-speech,arch-MuReNN,init_id-3.sbatch"
 sbatch "jobs/main/main,domain-speech,arch-MuReNN,init_id-4.sbatch"
+sbatch "jobs/main/main,domain-synth,arch-TDFilterbank,init_id-0.sbatch"
+sbatch "jobs/main/main,domain-synth,arch-TDFilterbank,init_id-1.sbatch"
+sbatch "jobs/main/main,domain-synth,arch-TDFilterbank,init_id-2.sbatch"
+sbatch "jobs/main/main,domain-synth,arch-TDFilterbank,init_id-3.sbatch"
+sbatch "jobs/main/main,domain-synth,arch-TDFilterbank,init_id-4.sbatch"
+sbatch "jobs/main/main,domain-synth,arch-LEAF,init_id-0.sbatch"
+sbatch "jobs/main/main,domain-synth,arch-LEAF,init_id-1.sbatch"
+sbatch "jobs/main/main,domain-synth,arch-LEAF,init_id-2.sbatch"
+sbatch "jobs/main/main,domain-synth,arch-LEAF,init_id-3.sbatch"
+sbatch "jobs/main/main,domain-synth,arch-LEAF,init_id-4.sbatch"
+sbatch "jobs/main/main,domain-synth,arch-MuReNN,init_id-0.sbatch"
+sbatch "jobs/main/main,domain-synth,arch-MuReNN,init_id-1.sbatch"
+sbatch "jobs/main/main,domain-synth,arch-MuReNN,init_id-2.sbatch"
+sbatch "jobs/main/main,domain-synth,arch-MuReNN,init_id-3.sbatch"
+sbatch "jobs/main/main,domain-synth,arch-MuReNN,init_id-4.sbatch"
+sbatch "jobs/main/main,domain-urban,arch-TDFilterbank,init_id-0.sbatch"
+sbatch "jobs/main/main,domain-urban,arch-TDFilterbank,init_id-1.sbatch"
+sbatch "jobs/main/main,domain-urban,arch-TDFilterbank,init_id-2.sbatch"
+sbatch "jobs/main/main,domain-urban,arch-TDFilterbank,init_id-3.sbatch"
+sbatch "jobs/main/main,domain-urban,arch-TDFilterbank,init_id-4.sbatch"
+sbatch "jobs/main/main,domain-urban,arch-LEAF,init_id-0.sbatch"
+sbatch "jobs/main/main,domain-urban,arch-LEAF,init_id-1.sbatch"
+sbatch "jobs/main/main,domain-urban,arch-LEAF,init_id-2.sbatch"
+sbatch "jobs/main/main,domain-urban,arch-LEAF,init_id-3.sbatch"
+sbatch "jobs/main/main,domain-urban,arch-LEAF,init_id-4.sbatch"
+sbatch "jobs/main/main,domain-urban,arch-MuReNN,init_id-0.sbatch"
+sbatch "jobs/main/main,domain-urban,arch-MuReNN,init_id-1.sbatch"
+sbatch "jobs/main/main,domain-urban,arch-MuReNN,init_id-2.sbatch"
+sbatch "jobs/main/main,domain-urban,arch-MuReNN,init_id-3.sbatch"
+sbatch "jobs/main/main,domain-urban,arch-MuReNN,init_id-4.sbatch"
 
